69a470957fb7bd5509c9244532ccc87bd967da59
Nikola-Mircic <nikola123mircic@gmail.com>
2026-05-14 14:35:42 +0200

    Common project

---
+++ Common/EegMeta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class EegMeta
+    {
+        [DataMember]
+        public string ParticipantId { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public int TotalRows { get; set; }
+
+        [DataMember]
+        public string SchemaVersion { get; set; }
+    }
+}
---
+++ Common/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        ServiceResponse StartSession(EegMeta meta);
+
+        [OperationContract]
+        ServiceResponse PushSample(EegSample sample);
+
+        [OperationContract]
+        ServiceResponse EndSession();
+    }
+}
