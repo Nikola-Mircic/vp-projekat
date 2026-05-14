80c1f494e338af552515e0885d21f27e91a8ef9d
Nikola-Mircic <nikola123mircic@gmail.com>
2026-05-14 15:12:35 +0200

    Server domain interfaces

---
+++ Server/Domain/ISessionStorage.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Domain
+{
+    internal interface ISessionStorage : IDisposable
+    {
+        bool StartSession(EegMeta meta);
+        bool PushSample(EegSample sample);
+    }
+}
